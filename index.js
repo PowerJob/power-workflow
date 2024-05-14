8afe23492970838d86a9981fa11248defb195a14
邓亮 <787615673@qq.com>
2024-05-14 17:09:34 +0800

    fix: 导出路径问题

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/help.min.js');
+  module.exports = require('./dist/power-workflow.min.js');
 } else {
-  module.exports = require('./dist/help.js');
+  module.exports = require('./dist/power-workflow.js');
 }
